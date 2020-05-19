9fb8e242a7bb98ff6a866a2bb044831e4c42afe6
alinvip22@gmail.com <HemulGM>
2020-05-19 11:44:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RESTHandler
+ RESTHandler
---
+++ REST.Handler.pas
@@ -0,0 +1,354 @@
+unit REST.Handler;
+
+interface
+
+uses
+  System.Classes, System.SysUtils,
+  {$IF DECLARED(FireMonkeyVersion)}
+  FMX.Types, FMX.Forms,
+  {$ELSE}
+  Vcl.Forms,
+  {$ENDIF}
+  REST.Client, REST.Json, JSON;
+
+type
+  THandlerException = class(Exception);
+
+  TParam = TArray<string>;
+
+  TParams = TArray<TParam>;
+
+  TResponseError = record
+    Code: Integer;
+    Text: string;
+  end;
+
+  TResponse = record
+    Success: Boolean;
+    Response: string;
+    JSON: string;
+    Error: TResponseError;
+    function GetJSONValue: TJSONValue;
+    function GetJSONResponse: TJSONValue;
+  end;
+
+  TOnHandlerError = procedure(Sender: TObject; E: Exception; Code: Integer; Text: string) of object;
+
+  TOnHandlerLog = procedure(Sender: TObject; const Value: string) of object;
+
+  TOnHandlerExecute = function(Sender: TObject; Request: TRESTRequest): TResponse;
+
+  TRequestConstruct = class
+    class var
+      Client: TRESTClient;
+  public
+    class function Request(Resource: string; Params: TParams): TRESTRequest;
+  end;
+
+  TRESTHandler = class
+  private
+    FStartRequest: Cardinal;
+    FRequests: Integer;
+    FRESTClient: TRESTClient;
+    FOnError: TOnHandlerError;
+    FOnLog: TOnHandlerLog;
+    FUseServiceKeyOnly: Boolean;
+    FOwner: TObject;
+    FExecuting: Integer;
+    FFullLog: Boolean;
+    FRequestLimitPerSecond: Integer;
+    FUsePseudoAsync: Boolean;
+    FOnExecute: TOnHandlerExecute;
+    FQueueWaits: Integer;
+    function FExecute(Request: TRESTRequest): TResponse;
+    function GetExecuting: Boolean;
+    function FOnExecuted(Request: TRESTRequest): TResponse;
+    procedure Queue;
+    procedure FLog(const Value: string);
+    procedure ProcError(E: Exception); overload;
+    procedure SetOnError(const Value: TOnHandlerError);
+    procedure SetOnLog(const Value: TOnHandlerLog);
+    procedure SetUseServiceKeyOnly(const Value: Boolean);
+    procedure SetOwner(const Value: TObject);
+    procedure SetFullLog(const Value: Boolean);
+    procedure SetRequestLimitPerSecond(const Value: Integer);
+    procedure SetUsePseudoAsync(const Value: Boolean);
+    procedure SetOnExecute(const Value: TOnHandlerExecute);
+    procedure InternalExecute(Request: TRESTRequest);
+  public
+    constructor Create(AOwner: TObject);
+    destructor Destroy; override;
+    procedure Log(Sender: TObject; const Text: string);
+    //
+    property Owner: TObject read FOwner write SetOwner;
+    //
+    function Execute(Request: string; Params: TParams): TResponse; overload;
+    function Execute(Request: string; Param: TParam): TResponse; overload;
+    function Execute(Request: string): TResponse; overload;
+    function Execute(Request: TRESTRequest; FreeRequset: Boolean = False): TResponse; overload;
+    //
+    property OnError: TOnHandlerError read FOnError write SetOnError;
+    property OnLog: TOnHandlerLog read FOnLog write SetOnLog;
+    property OnExecute: TOnHandlerExecute read FOnExecute write SetOnExecute;
+    //
+    property Client: TRESTClient read FRESTClient;
+    property Executing: Boolean read GetExecuting;
+    property QueueWaits: Integer read FQueueWaits;
+    //
+    property UseServiceKeyOnly: Boolean read FUseServiceKeyOnly write SetUseServiceKeyOnly;
+    property UsePseudoAsync: Boolean read FUsePseudoAsync write SetUsePseudoAsync;
+    property FullLog: Boolean read FFullLog write SetFullLog;
+    property RequestLimitPerSecond: Integer read FRequestLimitPerSecond write SetRequestLimitPerSecond;
+  end;
+
+implementation
+
+procedure WaitTime(MS: Int64);
+begin
+  if MS <= 0 then
+    Exit;
+  MS := TThread.GetTickCount + MS;
+  while MS > TThread.GetTickCount do
+    Sleep(100);
+end;
+
+{ TRequsetConstruct }
+
+class function TRequestConstruct.Request(Resource: string; Params: TParams): TRESTRequest;
+var
+  Param: TParam;
+begin
+  Result := TRESTRequest.Create(nil);
+  Result.Client := Client;
+  Result.Resource := Resource;
+  Result.Response := TRESTResponse.Create(Result);
+  for Param in Params do
+  begin
+    if not Param[0].IsEmpty then
+      Result.Params.AddItem(Param[0], Param[1]);
+  end;
+end;
+
+{ TRESTHandler }
+
+constructor TRESTHandler.Create(AOwner: TObject);
+begin
+  inherited Create;
+  FOwner := AOwner;
+  FExecuting := 0;
+  FStartRequest := 0;
+  FQueueWaits := 0;
+  FRequests := 0;
+  FUsePseudoAsync := True;
+  FRequestLimitPerSecond := 3;
+  FRESTClient := TRESTClient.Create(nil);
+  FRESTClient.Accept := 'application/json, text/plain; q=0.9, text/html;q=0.8,';
+  FRESTClient.AcceptCharset := 'UTF-8, *;q=0.8';
+
+  TRequestConstruct.Client := FRESTClient;
+end;
+
+destructor TRESTHandler.Destroy;
+begin
+  FRESTClient.Free;
+  inherited;
+end;
+
+function TRESTHandler.Execute(Request: string; Param: TParam): TResponse;
+begin
+  Result := Execute(TRequestConstruct.Request(Request, [Param]), True);
+end;
+
+function TRESTHandler.Execute(Request: string; Params: TParams): TResponse;
+begin
+  Result := Execute(TRequestConstruct.Request(Request, Params), True);
+end;
+
+function TRESTHandler.Execute(Request: string): TResponse;
+begin
+  Result := Execute(TRequestConstruct.Request(Request, []), True);
+end;
+
+function TRESTHandler.Execute(Request: TRESTRequest; FreeRequset: Boolean): TResponse;
+begin
+  try
+    Inc(FExecuting);
+    Result := FExecute(Request);
+  finally
+    if FreeRequset then
+      Request.Free;
+    Dec(FExecuting);
+  end;
+end;
+
+procedure TRESTHandler.Queue;
+begin
+  //Без очереди
+  if FRequestLimitPerSecond <= 0 then
+    Exit;
+  FRequests := FRequests + 1;
+  //Если был запрос
+  if FStartRequest > 0 then
+  begin
+    Inc(FQueueWaits);
+    //Если предел запросов
+    if FRequests > FRequestLimitPerSecond then
+    begin
+      FRequests := 0;
+      WaitTime(1000 - Int64(TThread.GetTickCount - FStartRequest));
+    end;
+    Dec(FQueueWaits);
+  end;
+  FStartRequest := TThread.GetTickCount;
+end;
+
+procedure TRESTHandler.InternalExecute(Request: TRESTRequest);
+begin
+  Queue;
+  try
+    Request.Execute;
+  except
+  end;
+end;
+
+function TRESTHandler.FExecute(Request: TRESTRequest): TResponse;
+var
+  IsDone: Boolean;
+begin
+  Result.Success := False;
+  if FFullLog then
+    FLog(Request.GetFullRequestURL);
+  try
+    IsDone := False;
+    //Если вызов из главного потока, то создаем поток и ждем выполнения через Application.ProcessMessages
+    if FUsePseudoAsync and (TThread.Current.ThreadID = MainThreadID) then
+    begin
+      with TThread.CreateAnonymousThread(
+        procedure
+        begin
+          InternalExecute(Request);
+          IsDone := True;
+        end) do
+      begin
+        FreeOnTerminate := False;
+        Start;
+        while (not IsDone) and (not Finished) do
+          Application.ProcessMessages;
+        Free;
+      end;
+    end
+    else
+    begin
+      InternalExecute(Request);
+      IsDone := True;
+    end;
+
+    if (not Application.Terminated) and IsDone then
+    begin
+      if FFullLog then
+        FLog(Request.Response.JSONText);
+
+      Result := FOnExecuted(Request);
+    end;
+  except
+    on E: Exception do
+      ProcError(E);
+  end;
+end;
+
+function TRESTHandler.FOnExecuted(Request: TRESTRequest): TResponse;
+begin
+  if Assigned(FOnExecute) then
+    Result := FOnExecute(Self, Request)
+  else
+  begin
+    Result.JSON := Request.Response.JSONText;
+    Result.Success := True;
+  end;
+end;
+
+procedure TRESTHandler.FLog(const Value: string);
+begin
+  if Assigned(FOnLog) then
+    FOnLog(Self, Value);
+end;
+
+function TRESTHandler.GetExecuting: Boolean;
+begin
+  Result := FExecuting > 0;
+end;
+
+procedure TRESTHandler.Log(Sender: TObject; const Text: string);
+begin
+  if Assigned(FOnLog) then
+    FOnLog(Sender, Text);
+end;
+
+procedure TRESTHandler.SetOnError(const Value: TOnHandlerError);
+begin
+  FOnError := Value;
+end;
+
+procedure TRESTHandler.SetOnExecute(const Value: TOnHandlerExecute);
+begin
+  FOnExecute := Value;
+end;
+
+procedure TRESTHandler.SetOnLog(const Value: TOnHandlerLog);
+begin
+  FOnLog := Value;
+end;
+
+procedure TRESTHandler.SetOwner(const Value: TObject);
+begin
+  FOwner := Value;
+end;
+
+procedure TRESTHandler.SetRequestLimitPerSecond(const Value: Integer);
+begin
+  FRequestLimitPerSecond := Value;
+end;
+
+procedure TRESTHandler.SetUsePseudoAsync(const Value: Boolean);
+begin
+  FUsePseudoAsync := Value;
+end;
+
+procedure TRESTHandler.SetUseServiceKeyOnly(const Value: Boolean);
+begin
+  FUseServiceKeyOnly := Value;
+end;
+
+procedure TRESTHandler.SetFullLog(const Value: Boolean);
+begin
+  FFullLog := Value;
+end;
+
+procedure TRESTHandler.ProcError(E: Exception);
+begin
+  if Assigned(FOnError) then
+    FOnError(Self, THandlerException.Create(E.Message), 0, E.Message);
+end;
+
+{ TResponse }
+
+{$WARNINGS OFF}
+function TResponse.GetJSONValue: TJSONValue;
+begin
+  if not JSON.IsEmpty then
+    Result := TJSONObject.ParseJSONValue(UTF8ToString(JSON))
+  else
+    Result := nil;
+end;
+
+function TResponse.GetJSONResponse: TJSONValue;
+begin
+  if not Response.IsEmpty then
+    Result := TJSONObject.ParseJSONValue(UTF8ToString(Response))
+  else
+    Result := nil;
+end;
+{$WARNINGS ON}
+
+end.
+
